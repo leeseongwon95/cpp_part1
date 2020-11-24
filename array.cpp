e3c5d0a4bae7e715c2992db2ff19232c67b1c7f4
Lee Seong Won <syanggu@naver.com>
2020-11-24 19:50:27 +0900

    Array

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+/*
+  do while 문 : 반복문의 한 종류
+  형태 : do { } while(조건식); 의 형태로 구성
+  while 문은 처음 진입부터 조건식을 체크하지만 do while 은 처음 한번은 무조건 동작이 되고
+  그 후에 조건식을 체크해서 true 일 경우 동작되는 반복문이다.
+*/
+do {
+
+} while (true); 
+
+  return 0;
+}
